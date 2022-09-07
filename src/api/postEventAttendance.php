6985fab891548d67b19beec02870413019b668e5
kohei23n <kohei.s.n@gmail.com>
2022-09-07 13:31:18 +0900

    add：テーブルにカラム追加

--- src/api/postEventAttendance.php
+++ src/api/postEventAttendance.php
@@ -4,8 +4,18 @@ header('Content-Type: application/json; charset=UTF-8');
 
 $eventId = $_POST['eventId'];
 $userId = $_POST['userId'];
+$status = $_POST['status'];
 
-if ($eventId > 0) {
-  $stmt = $db->prepare('INSERT INTO event_attendance SET event_id=?, user_id = ?');
-  $stmt->execute(array($eventId, $userId));
+$sql = 'SELECT COUNT(user_id) FROM event_attendance WHERE user_id = ? AND event_id = ?';
+$stmt = $db->prepare($sql);
+$stmt->execute(array($userId, $eventId));
+$number = $stmt->fetch();
+
+
+if ($eventId > 0 && $number = 0) {
+  $stmt = $db->prepare('INSERT INTO event_attendance SET event_id=?, user_id = ?, status = ?');
+  $stmt->execute(array($eventId, $userId, $status));
+} else {
+  $stmt = $db->prepare('UPDATE event_attendance SET user_id = ?, status = ? WHERE event_id = ?');
+  $stmt->execute(array($userId, $status, $eventId));
 }
