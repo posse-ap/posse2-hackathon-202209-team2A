b624574e64700246c85fa84d4d46286f1517b1f2
kohei23n <kohei.s.n@gmail.com>
2022-09-08 03:36:14 +0900

    いらない差分削除

--- src/js/main.js
+++ src/js/main.js
@@ -89,19 +89,6 @@ async function openModal(eventId) {
         break;
     }
     modalInnerHTML.insertAdjacentHTML('afterbegin', modalHTML)
-
-
-    // アコーディオン
-    $(function(){
-      //.accordion1の中のp要素がクリックされたら
-      $('.accordion a').click(function(){
-          //クリックされた.accordion1の中のp要素に隣接するul要素が開いたり閉じたりする。
-          $(this).next('ul').slideToggle();
-      });
-  });
-
-
-
   } catch (error) {
     console.log(error)
   }
