e54e13d6b19f35aacf629a684202306edd830aef
kohei23n <kohei.s.n@gmail.com>
2022-09-08 01:23:12 +0900

    fix：いらない差分削除

--- src/admin.php
+++ src/admin.php
@@ -4,19 +4,6 @@ require('dbconnect.php');
 $stmt = $db->query('SELECT events.id, events.name, events.start_at, events.end_at, count(event_attendance.id) AS total_participants FROM events LEFT JOIN event_attendance ON events.id = event_attendance.event_id GROUP BY events.id');
 $events = $stmt->fetchAll();
 
-$user_id = $_SESSION['user_id'];
-$status = filter_input(INPUT_GET, 'status');
-
-$stmt = $db->query('SELECT events.id, events.name, events.start_at, events.end_at FROM events LEFT JOIN event_attendance ON events.id = event_attendance.event_id GROUP BY events.id  ORDER BY start_at ASC');
-$stmt->execute();
-$events = $stmt->fetchAll();
-
-function get_day_of_week($w)
-{
-  $day_of_week_list = ['日', '月', '火', '水', '木', '金', '土'];
-  return $day_of_week_list["$w"];
-}
-
 ?>
 
 <!DOCTYPE html>
