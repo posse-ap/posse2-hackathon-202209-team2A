af585c84ce05c4bd66af80846d786f9b30cf9c12
kohei23n <kohei.s.n@gmail.com>
2022-09-08 01:45:39 +0900

    add：fix

--- src/event_edit.php
+++ src/event_edit.php
@@ -28,50 +28,40 @@ if (isset($_POST['submit'])) {
 <html>
 
 <head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet">
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <title>Schedule | POSSE</title>
 </head>
 
 <body>
-  <div class="change">
-    <form action="" method="post" id="postForm">
-      <div>
-        <label for="event_name" id="event_name">イベント名</label>
-        <input type="text" name="event_name" value="<?= $event['name'] ?>">
+  <header class="h-16">
+    <div class="flex justify-between items-center w-full h-full mx-auto pl-2 pr-5">
+      <div class="h-full">
+        <img src="/img/header-logo.png" alt="" class="h-full">
       </div>
-      <div>
-        <label for="event_start">開始日時</label>
-        <input type="datetime-local" name="event_start" value="<?= $event['start_at'] ?>">
-      </div>
-      <div>
-        <label for="event_end">終了日時</label>
-        <input type="datetime-local" name="event_end" value="<?= $event['end_at'] ?>">
-      </div>
-      <div>
-        <label for="event_detail">イベント詳細</label>
-        <input type="text" name="event_detail" value="<?= $event['detail'] ?>">
-      </div>
-      <input type="submit" value="編集" name="submit">
-    </form>
-  </div>
-  </div>
-</body>
-
-<body>
-  <div>
-    <div>
-      <h2>イベント編集</h2>
     </div>
-    <form action="" method="post" id="postForm">
-      <p>イベント名</p>
-      <input type="text" name="event_name" value="<?= $event['name'] ?>">
-      <p>開始日時</p>
-      <input type="datetime-local" name="event_start" value="<?= $event['start_at'] ?>">
-      <p>終了日時</p>
-      <input type="datetime-local" name="event_end" value="<?= $event['end_at'] ?>">
-      <p>イベント詳細</p>
-      <input type="text" name="event_detail" value="<?= $event['detail'] ?>">
-      <input type="submit" value="送信" name="submit">
+  </header>
+  <div class="bg-gray-100 p-4">
+    <div>
+      <h2 class="text-2xl mt-3 mb-3 font-bold">イベント編集</h2>
+    </div>
+  </div>
+  <div class="change">
+    <form action="" method="post" id="postForm" class="p-3">
+      <p class="text-xl pt-4 pb-4">イベント名</p>
+      <input type="text" name="event_name" class="w-full" value="<?= $event['name'] ?>">
+      <p class="text-xl pt-5 pb-4">開始日時</p>
+      <input type="datetime-local" name="event_start" class="w-full" value="<?= $event['start_at'] ?>">
+      <p class="text-xl pt-5 pb-4">終了日時</p>
+      <input type="datetime-local" name="event_end" class="w-full" value="<?= $event['end_at'] ?>">
+      <p class="text-xl pt-5 pb-4">イベント詳細</p>
+      <input type="text" name="event_detail" class="w-full" value="<?= $event['detail'] ?>">
+      <input type="submit" value="送信" name="submit" class="cursor-pointer w-4/5 p-3 m-10 text-xl text-white bg-blue-400 rounded-3xl bg-gradient-to-r from-blue-600 to-blue-300 flex items-center justify-center">
     </form>
+  </div>
   </div>
 </body>
 
