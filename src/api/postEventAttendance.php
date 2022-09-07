151a0ecb1a67fef0db675159d377c064754621f4
kohei23n <kohei.s.n@gmail.com>
2022-09-07 11:46:48 +0900

    差分削除

--- src/api/postEventAttendance.php
+++ src/api/postEventAttendance.php
@@ -6,7 +6,6 @@ $eventId = $_POST['eventId'];
 $userId = $_POST['userId'];
 
 if ($eventId > 0) {
-  #12 event_id とともに user_id を入力。後から user_id に動的な値を入れたい
   $stmt = $db->prepare('INSERT INTO event_attendance SET event_id=?, user_id = ?');
   $stmt->execute(array($eventId, $userId));
 }
