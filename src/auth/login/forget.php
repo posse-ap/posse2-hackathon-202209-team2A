f39f911682599f99f32ebf4dd4f27c8da40e7613
kohei23n <kohei.s.n@gmail.com>
2022-09-07 18:14:56 +0900

    fix：文字化け修正

--- src/auth/login/forget.php
+++ src/auth/login/forget.php
@@ -35,9 +35,9 @@ if (isset($_POST['submit_email'])) {
     新しいパスワードをご登録ください。
     パスワードリセットの申請に心当たりがない場合は、以降の対応は不要となります。
     パスワードの再設定URL：
-    http://localhost/auth/login/reset.php?pass_reset=$passResetToken
+    http://localhost/auth/login/reset.php?pass_reset=${$passResetToken}
     ";
-    $headers = "From: posse-ap";
+    $headers = ["From" => "posse-ap", "Content-Type" => "text/plain; charset=UTF-8", "Content-Transfer-Encoding" => "8bit"];;
 
     mb_send_mail($to, $subject, $message, $headers);
 
@@ -47,7 +47,6 @@ if (isset($_POST['submit_email'])) {
   } else {
     $err_msg = "メールアドレスが登録されていません。";
   }
-
 }
 
 
