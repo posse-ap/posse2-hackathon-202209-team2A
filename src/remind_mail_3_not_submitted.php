7ce0f2d0d50160600cb287641911551d2851ff53
冨永桃 <momotominaga@tominagamomonoMacBook-Pro.local>
2022-09-08 20:50:06 +0900

    ３日後中に行われるイベントのみを取得同時にuserにおいて、未回答者のみ取得

--- src/remind_mail_3_not_submitted.php
+++ src/remind_mail_3_not_submitted.php
@@ -11,7 +11,7 @@ mb_internal_encoding('UTF-8');
 $tomorrow_start  = date('Y-m-d 00:00:00', strtotime("+3 day"));
 $tomorrow_end  = date('Y-m-d 23:59:59', strtotime("+3 day"));
 
-// ３日後中に行われるイベントのみを取得 同時にuserにおいて、未回答者のみ取得
+// SELECT events.name, events.start_at, users.name,event_attendance.status FROM events LEFT JOIN event_attendance ON event_attendance.event_id = events.id LEFT JOIN users ON event_attendance.user_id = users.id WHERE events.start_at >= CURDATE() AND event_attendance.status='presence' ORDER BY events.name DESC;
 $stmt = $db->prepare("SELECT *
     FROM events 
     CROSS JOIN users 
